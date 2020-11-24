53cf61184a64b64d448781cd17e8cc12d331971a
leotag-source <74937700+leotag-source@users.noreply.github.com>
2020-11-24 16:11:33 +0200

    Update system.h

--- system.h
+++ system.h
@@ -1,1 +1,26 @@
+#pragma option OST
+#pragma option ON
+#pragma option cri-
+#pragma option -CPA
+#initallvar 0
+#jumptomain FALSE
+#startaddress 0
+#code32 TRUE
 
+char   os_name[8]   = {'M','E','N','U','E','T','0','1'};
+dword  os_version   = 0x00000001;
+dword  start_addr   = #______INIT______;
+dword  final_addr   = #______STOP______+32;
+dword  alloc_mem    = 100000;
+dword  x86esp_reg   = 100000;
+dword  I_Param      = #param;
+dword  I_Path       = #program_path;
+char param[4096];
+char program_path[4096];
+
+void ______INIT______()
+{
+  
+}
+
+______STOP______:
